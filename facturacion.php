ef7320910ecc657a043797ce80d44548bd065696
eduard-mazo <eduard.mazo@gmail.com>
2017-06-05 23:36:17 -0500

    Balance changes
    receipt template!

--- facturacion.php
+++ facturacion.php
@@ -2,8 +2,42 @@
 <html>
 <head>
 	<title>FACTURACION</title>
+	<link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:200,800" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+	<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+	<script src="js/menu.js"></script>
+	
 </head>
 <body>
+	<i class="fa fa-bars toggle_menu"></i>
+	
+	<div class="sidebar_menu">
+		<i class="fa fa-times"></i>
+		<center>
+			<a href="index.php"><h1 class="boxed_item">EDISON <span class="logo_bold">VELASQUEZ</span></h1></a>
+			<h2 class="logo_title">DROGUERIA LUNA</h2>
+		</center>
+		<ul class="navigation_selection">
+			<li ><a class="navigation_item" href="perfil.php" 		>PERFIL</a></li>
+			<li ><a class="navigation_item" href="facturacion.php"	>FACTURACION</a></li>
+			<li ><a class="navigation_item" href="balance.php"		>BALANCE</a></li>
+			<li ><a class="navigation_item" href="faltantes.php"	>FALTANTES</a></li>
+		</ul>
 
+		<center>
+			<a href="index.php">
+				<h1 class="letra">
+				<i class="fa fa-sign-in login"></i>
+				SING UP</h1>
+				</a>	
+		
+	
+			
+		</center>
+		
+
+	</div>
 </body>
 </html>
